626243129fe765ac62017901fd4485b1daa80bcc
mehedititu <mh.me.mehedi@gmail.com>
2023-02-13 19:38:41 +0600

    first commit

--- index.js
+++ index.js
@@ -9,49 +9,73 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q66zrl2.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k4gmzpi.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
 });
 
 const run = async () => {
-  try {
-    const db = client.db("moontech");
-    const productCollection = db.collection("product");
+    try {
+        const db = client.db("moontech");
+        const blogCollection = db.collection("blog");
 
-    app.get("/products", async (req, res) => {
-      const cursor = productCollection.find({});
-      const product = await cursor.toArray();
+        app.get("/blogs", async (req, res) => {
+            const cursor = blogCollection.find({});
+            const blog = await cursor.toArray();
 
-      res.send({ status: true, data: product });
-    });
+            res.send({ status: true, data: blog });
+        });
 
-    app.post("/product", async (req, res) => {
-      const product = req.body;
+        app.get("/blog/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await blogCollection.findOne(query);
+            res.send(result);
+        });
 
-      const result = await productCollection.insertOne(product);
+        app.post("/blog", async (req, res) => {
+            const blog = req.body;
 
-      res.send(result);
-    });
+            const result = await blogCollection.insertOne(blog);
 
-    app.delete("/product/:id", async (req, res) => {
-      const id = req.params.id;
+            res.send(result);
+        });
 
-      const result = await productCollection.deleteOne({ _id: ObjectId(id) });
-      res.send(result);
-    });
-  } finally {
-  }
+        app.delete("/blog/:id", async (req, res) => {
+            const id = req.params.id;
+
+            const result = await blogCollection.deleteOne({
+                _id: ObjectId(id),
+            });
+            res.send(result);
+        });
+
+        app.put("/blog/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: req.body,
+            };
+            const result = await blogCollection.updateOne(
+                filter,
+                updateDoc,
+                options
+            );
+            res.send(result);
+        });
+    } finally {
+    }
 };
 
 run().catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Redux assignment Running on");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Redux assignment is running on ${port}`);
 });
